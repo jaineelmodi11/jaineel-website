04c26534a7c05ddb33da96254bffda9755b7010c
jaineelmodi11 <79771890+jaineelmodi11@users.noreply.github.com>
2025-06-10 19:21:05 -0400

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'export',
+  basePath: '/jaineel-website',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 export default nextConfig
